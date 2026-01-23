73ac67aa850c41479d40cc42f6f0520dfb9b9481
AravindSama <Aravind.sama12@gmail.com>
2026-01-23 08:40:18 -0600

    OpenWeather Twilio Stock News newsAPI alphavantage

--- OpenWeatherTwilio/main.py
+++ OpenWeatherTwilio/main.py
@@ -4,14 +4,14 @@ import requests
 from twilio.rest import Client
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = "__YOUR_OWM_API_KEY__"
+api_key_openweather = ""
 account_sid = "__YOUR_TWILIO_ACCOUNT_ID__"
 auth_token = "__YOUR_TWILIO_AUTH_TOKEN__"
 
 weather_params = {
-    "lat": 46.947975,
-    "lon": 7.447447,
-    "appid": api_key,
+    "lat": 36.55,
+    "lon": 31.04,
+    "appid": api_key_openweather,
     "cnt": 4,
 }
 
@@ -26,11 +26,4 @@ for hour_data in weather_data["list"]:
     if int(condition_code) < 700:
         will_rain = True
 if will_rain:
-    client = Client(account_sid, auth_token)
-    message = client.messages \
-        .create(
-        body="It's going to rain today. Remember to bring an ☔️",
-        from_="YOUR TWILIO VIRTUAL NUMBER",
-        to="YOUR TWILIO VERIFIED REAL NUMBER"
-    )
-    print(message.status)
+    print("It's going to rain today. Remember to bring an ☔️")
